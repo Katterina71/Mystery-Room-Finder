19cea9785aeae557c307d2618e480605db48f147
Kate Iliu <beviatori@gmail.com>
2024-04-08 16:34:51 -0600

    Create functiondo add items on image

--- src/game.js
+++ src/game.js
@@ -2,20 +2,19 @@ class Room {
     constructor (house,name,imagePath){
         this.name = name
         this.imagePath = imagePath
-        this.house = house;
+        this.house = house
+        this.room = []
     }
 }
 
 
 class Item {
-    constructor (name,imagePath,x,y){
+    constructor (name,imagePath,leftPX,topPX){
         this.name = name
         this.imagePath = imagePath
         this.found = false
-        this.lostColor = "light blue"
-        this.findColor = "green"
-        this.coordinateX = x;
-        this.coordinateY = y;
+        this.leftPX = leftPX;
+        this.topPX = topPX;
     }
     soundOn(sound) {
         return true;
@@ -26,9 +25,10 @@ class Item {
 }
 
 class BonusItem extends Item {
-    constructor(name) {
-        bonus = true
-        score = 30
+    constructor () {
+        super();
+        this.bonus = true
+        this.score = 30
     }
 }
 
@@ -49,8 +49,8 @@ class ItemList {
         this.roomName = name;
         this.roomItems = [];
     }
-    createItemList(name){
-        const room = new Item(name,imagePath,x,y);
+    createItemList(name,imagePath,leftPX,topPX){
+        const room = new Item(name,imagePath,leftPX,topPX);
         this.roomItems.push(room);
         return this.roomItems;
     }
@@ -78,8 +78,32 @@ class Player {
 
 
 const house = new RoomList("louie");
-const louieRoom = house.createRoom("louieRoom","./img/room1/louieroom.svg");
-console.log(louieRoom[0]);
+const houseLouie = house.createRoom("louieRoom","./img/room1/louieroom.svg");
+
+
+const itemsLouieRoom = new ItemList("LouieItems");
+itemsLouieRoom.createItemList("chair","./img/room1/items/chair.svg",320,-392)
+itemsLouieRoom.createItemList("box","./img/room1/items/box.svg",413,-765)
+itemsLouieRoom.createItemList("frame","./img/room1/items/frame.svg",660,-716)
+itemsLouieRoom.createItemList("pencil","./img/room1/items/pencil.svg",472,-506)
+itemsLouieRoom.createItemList("pillow","./img/room1/items/pillow.svg",166,-645)
+itemsLouieRoom.createItemList("plant","./img/room1/items/plant.svg",0,-753)
+itemsLouieRoom.createItemList("sofa","./img/room1/items/sofa.svg",480,-850)
+itemsLouieRoom.createItemList("nightstand","./img/room1/items/nightstand.svg",630,-972)
+itemsLouieRoom.createItemList("backpack","./img/room1/items/backpack.svg",538,-1173)
+itemsLouieRoom.createItemList("scarf","./img/room1/items/scarf.svg",478,-1348)
+itemsLouieRoom.createItemList("bedsidelamp","./img/room1/items//bedsidelamp.svg",678,-1565)
+itemsLouieRoom.createItemList("book","./img/room1/items/book.svg",640,-1592)
+itemsLouieRoom.createItemList("umbrella","./img/room1/items/umbrella.svg",635,1620)
+const spider = new BonusItem ("spider","./img/room1/items/spider.svg",420,-2150)
+
+
+houseLouie[0].room.push(itemsLouieRoom);
+
+console.log(houseLouie[0].room);
+console.log(houseLouie[0].room[0].roomItems); // array of items
+
+
 
 
 
@@ -87,6 +111,7 @@ console.log(louieRoom[0]);
 const startGame = document.getElementById("playerField");
 console.log(startGame)
 const gameFiledIMG = document.getElementById("gameFiledIMG");
+const gamePicture = document.getAnimations("dynamicIMG")
 console.log(gameFiledIMG)
 
 startGame.addEventListener("click", function(event){
@@ -94,6 +119,14 @@ startGame.addEventListener("click", function(event){
     const clickElement = event.target; 
     if (clickElement.nodeName = "BUTTON") {
         document.getElementById('gameFiledIMG').src = louieRoom[0].imagePath;
+        const itemsArr = houseLouie[0].room[0].roomItems;
+        for (let i in length (itemsArr)) {
+            let imgDiv = document.createElement('div');
+            gamePicture.appendChild(imgDiv);
+            let itemImg = document.createElement('img');
+
+
+        }
     }
 })
 
