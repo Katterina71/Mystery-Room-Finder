35af1212ea83a2d4c61cf575391d069fc7066898
Kate Iliu <beviatori@gmail.com>
2024-04-08 20:55:14 -0600

    Dynamic add items on picture

--- src/game.js
+++ src/game.js
@@ -82,24 +82,29 @@ const houseLouie = house.createRoom("louieRoom","./img/room1/louieroom.svg");
 
 
 const itemsLouieRoom = new ItemList("LouieItems");
-itemsLouieRoom.createItemList("chair","./img/room1/items/chair.svg",320,-392)
-itemsLouieRoom.createItemList("box","./img/room1/items/box.svg",413,-765)
-itemsLouieRoom.createItemList("frame","./img/room1/items/frame.svg",660,-716)
-itemsLouieRoom.createItemList("pencil","./img/room1/items/pencil.svg",472,-506)
-itemsLouieRoom.createItemList("pillow","./img/room1/items/pillow.svg",166,-645)
-itemsLouieRoom.createItemList("plant","./img/room1/items/plant.svg",0,-753)
-itemsLouieRoom.createItemList("sofa","./img/room1/items/sofa.svg",480,-850)
-itemsLouieRoom.createItemList("nightstand","./img/room1/items/nightstand.svg",630,-972)
-itemsLouieRoom.createItemList("backpack","./img/room1/items/backpack.svg",538,-1173)
-itemsLouieRoom.createItemList("scarf","./img/room1/items/scarf.svg",478,-1348)
-itemsLouieRoom.createItemList("bedsidelamp","./img/room1/items//bedsidelamp.svg",678,-1565)
-itemsLouieRoom.createItemList("book","./img/room1/items/book.svg",640,-1592)
-itemsLouieRoom.createItemList("umbrella","./img/room1/items/umbrella.svg",635,1620)
-const spider = new BonusItem ("spider","./img/room1/items/spider.svg",420,-2150)
+itemsLouieRoom.createItemList("lamp","./img/room1/items/lamp.svg",411,-453)
+itemsLouieRoom.createItemList("box","./img/room1/items/box.svg",345,-625)
+itemsLouieRoom.createItemList("chair","./img/room1/items/chair.svg",172,-292)
+itemsLouieRoom.createItemList("frame","./img/room1/items/frame.svg",390,-530)
+itemsLouieRoom.createItemList("pencil","./img/room1/items/pencil.svg",122,-292)
+itemsLouieRoom.createItemList("pillow","./img/room1/items/pillow.svg",-215,-349)
+itemsLouieRoom.createItemList("plant","./img/room1/items/plant.svg",-473,-370)
+itemsLouieRoom.createItemList("sofa","./img/room1/items/sofa.svg",-15,-285)
+itemsLouieRoom.createItemList("nightstand","./img/room1/items/nightstand.svg",630,-442)
+itemsLouieRoom.createItemList("backpack","./img/room1/items/backpack.svg",412,-465)
+itemsLouieRoom.createItemList("scarf","./img/room1/items/scarf.svg",270,-485)
+itemsLouieRoom.createItemList("bedsidelamp","./img/room1/items//bedsidelamp.svg",445,-574)
+itemsLouieRoom.createItemList("book","./img/room1/items/book.svg",330,-574)
+itemsLouieRoom.createItemList("umbrella","./img/room1/items/umbrella.svg",215,-424)
+const spider = new BonusItem ("spider","./img/room1/items/spider.svg",-80,-900)
+
+
+
+
 
 
 houseLouie[0].room.push(itemsLouieRoom);
-
+console.log(houseLouie[0].imagePath);
 console.log(houseLouie[0].room);
 console.log(houseLouie[0].room[0].roomItems); // array of items
 
@@ -109,23 +114,30 @@ console.log(houseLouie[0].room[0].roomItems); // array of items
 
 // main 
 const startGame = document.getElementById("playerField");
-console.log(startGame)
 const gameFiledIMG = document.getElementById("gameFiledIMG");
-const gamePicture = document.getAnimations("dynamicIMG")
-console.log(gameFiledIMG)
+const gamePicture = document.getElementById("dynamicIMG")
+console.log(gamePicture)
+
+let itemsArr =[];
 
 startGame.addEventListener("click", function(event){
     event.preventDefault();
     const clickElement = event.target; 
     if (clickElement.nodeName = "BUTTON") {
-        document.getElementById('gameFiledIMG').src = louieRoom[0].imagePath;
+        document.getElementById('gameFiledIMG').src = houseLouie[0].imagePath;
+        console.log(houseLouie[0].imagePath);
         const itemsArr = houseLouie[0].room[0].roomItems;
-        for (let i in length (itemsArr)) {
-            let imgDiv = document.createElement('div');
-            gamePicture.appendChild(imgDiv);
+        for (let i in itemsArr) {
+            // debugger;
             let itemImg = document.createElement('img');
-
-
+            console.log(itemsArr[i].name);
+            console.log(itemsArr[i].imagePath);
+            itemImg.setAttribute('id', itemsArr[i].name);
+            itemImg.classList.add("overlay-image");
+            itemImg.src = itemsArr[i].imagePath;
+            itemImg.style.left = itemsArr[i].leftPX+'px';
+            itemImg.style.top = itemsArr[i].topPX+'px';
+            gamePicture.appendChild(itemImg);
         }
     }
 })
